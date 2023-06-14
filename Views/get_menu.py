dfba85482542d0c803334afd3b81f14b0cd805e2
Vilina <vilina.grafova@mail.ru>
2023-06-14 22:18:14 +0300

    Completed project

---
+++ Views/get_menu.py
@@ -0,0 +1,10 @@
+from Models.menu import menu_options
+
+
+def print_menu():
+    """
+    Функция отрисовки меню в консоль
+    :return:
+    """
+    for key in menu_options.keys():
+        print(key, '--', menu_options[key])
---
+++ main.py
@@ -0,0 +1,4 @@
+from Controllers.menu_controller import start_menu
+
+if __name__ == "__main__":
+    start_menu()
